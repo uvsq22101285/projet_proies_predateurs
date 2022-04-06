839378192b7a63978f2709e608ec559dd74a7c73
Eliott MOBRE <eliott.mobre@gmail.com>
2022-04-06 20:28:08 +0200

    o

--- test.py
+++ test.py
@@ -2,12 +2,12 @@
 from tkinter import *  
 from PIL import ImageTk,Image  
 
-root = Tk()  
+root = Tk()
 root.title('PythonGuides')
 root.geometry('500x500')
 
-canvas = Canvas(root, width = 500, height = 500)  
-canvas.pack()  
-img = ImageTk.PhotoImage(Image.open('terrain.png'))  
-canvas.create_image(10,10,anchor=NW, image=img) 
-root.mainloop() 
+canvas = Canvas(root, width = 500, height = 500)
+canvas.pack()
+img = ImageTk.PhotoImage(Image.open('terrain.png'))
+canvas.create_image(10,10,anchor=NW, image=img)
+root.mainloop()
