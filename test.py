ffd22e0545a202406c6b1051f3c8a78d426f50c5
Jean-Alexandre SUZANNE <jean-alexandre.suzanne@ens.uvsq.fr>
2022-04-06 21:38:11 +0200

    carré

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 '''TEST IMAGE'''
 from tkinter import *  
 from PIL import ImageTk,Image
-'''
-im = Image.open("sol.png")
+''''''''''
+im = Image.open("carré_sol.png")
 print(im.size)
 new_size = im.resize((128,128))
 new_size.show()
-'''
+'''''''''
 root = Tk()
 root.title('PythonGuides')
 root.geometry('500x500')
 
-canvas = Canvas(root, width = 640, height = 640,bg='blue')
-img = PhotoImage(file ="fox.png")
+canvas = Canvas(root, width = 640, height = 640,bg='green')
+img = PhotoImage(file ="carré_sol.png")
 for x in range(20):
     for y in range(20):
         canvas.create_image(x*32,y*32,image=img,anchor=NW)
