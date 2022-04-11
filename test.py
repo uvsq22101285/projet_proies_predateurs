21eb9ceff6d3e91875269665625b428d6d037ac4
Eliott MOBRE <eliott.mobre@gmail.com>
2022-04-11 15:47:49 +0200

    oui

--- test.py
+++ test.py
@@ -1,39 +1,61 @@
-'''TEST IMAGE'''
-from tkinter import *  
-from PIL import ImageTk,Image
-''''''''''
-im = Image.open("carré_sol.png")
-print(im.size)
-new_size = im.resize((32,32))
-new_size.show()
-'''''''''
+#Lib
+from random import randint
+from tkinter import * 
+
+#Tkinter
 root = Tk()
 root.title('PythonGuides')
 root.geometry('500x500')
+canvas = Canvas(root, width = 640, height = 640,bg='green')
 
-canvas = Canvas(root, width = 640, height = 640,bg='black')
-img = PhotoImage(file ="carré_sol.png")
-img2 = PhotoImage(file ="rabbit.png")
-img3 = PhotoImage(file ="fox.png")
-img4 = PhotoImage(file="tomato.png")
-img5 = PhotoImage(file="migrant.png")
-img6 = PhotoImage(file="police-officer-head.png")
-img7 = PhotoImage(file="water-flask.png")
-img8 = PhotoImage(file="rolling-bomb.png")
+coordF=[]
+coordP=[]
+#load img
+sol = PhotoImage(file ="carré_sol.png")
+rabbit = PhotoImage(file ="rabbit.png")
+fox = PhotoImage(file ="fox.png")
 
-for x in range(20):
-    for y in range(20):
-        canvas.create_image(x*64,y*64,image=img,anchor=NW)
+#Affichage case
+def affGrid(grid):
+    for x in range(10):
+        for y in range(10):
+            canvas.create_image(x*64,y*64,image=sol,anchor=NW)
+            if grid[x][y] ==1:
+                canvas.create_image(x*64,y*64,image=rabbit, anchor=NW)
+                coordF.append([x,y])
+            if grid[x][y] ==2:
+                canvas.create_image(x*64,y*64,image=fox, anchor=NW)
+                coordP.append([x,y])
+    canvas.grid()
 
-canvas.create_image(64,0, image=img3, anchor=NW)
-canvas.create_image(0,0,image=img2, anchor=NW)
-canvas.create_image(0,64,image=img4, anchor=NW)
-canvas.create_image(64,64,image=img5, anchor=NW)
-canvas.create_image(64,128,image=img6, anchor=NW)
-canvas.create_image(128,128,image=img7, anchor=NW)
-canvas.create_image(128,64,image=img8, anchor=NW)
+#fait apparaître animal X fois
+def SpawnA(grid,nbr,animal): 
+    for X in range(nbr):
+        r1 = [randint(0,9),randint(0,9)] #random case vide
+        while grid[r1[0]][r1[1]] !=0:
+            r1 = [randint(0,9),randint(0,9)]
+        grid[r1[0]][r1[1]] = animal
 
+def Start():
+    grid =[[0 for i in range(10)]for j in range(10)] #Besoin de supprimer les anciens avant
+    SpawnA(grid,1,1)
+    SpawnA(grid,1,2)
+    affGrid(grid)
+    find()
+Start
 
-canvas.grid()
+#déplacement Fox
+def find():
+    dx = coordF[0][0]-coordP[0][0]
+    dy = coordF[0][1]-coordP[0][1]
+    print(dx,dy)
+    dist=[dx,dy]
+    for i in range(2):
+        if dist[i]!=0:
+            dist[i]=dist[i]/abs(dist[i])
 
+    print(dist)
+
+BtnRandom = Button(root,text='Random', command=Start)
+BtnRandom.grid()
 root.mainloop()
