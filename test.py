1c1887490096075c931e057d58e130c2553384b5
Eliott MOBRE <eliott.mobre@gmail.com>
2022-04-16 19:58:41 +0200

    p

--- test.py
+++ test.py
@@ -20,7 +20,6 @@ coordP=[]
 global coordF
 coordF=[]
 
-
 #Affichage case
 def affGrid():
     global coordP
@@ -28,10 +27,10 @@ def affGrid():
     for x in range(10):
         for y in range(10):
             canvas.create_image(x*64,y*64,image=sol,anchor=NW)
-            if grid[x][y] ==1:
+            if grid[x][y]==1:
                 canvas.create_image(x*64,y*64,image=rabbit, anchor=NW)
                 coordP.append([x,y])
-            if grid[x][y] ==2:
+            if grid[x][y]==2:
                 canvas.create_image(x*64,y*64,image=fox, anchor=NW)
                 coordF.append([x,y])
     canvas.grid()
@@ -47,10 +46,9 @@ def SpawnA(nbr,animal):
 def Start():
     global grid
     grid =[[0 for i in range(10)]for j in range(10)] #Besoin de supprimer les anciens avant
-    SpawnA(3,1)
+    SpawnA(1,1)
     SpawnA(1,2)
     affGrid()
-Start()
 
 #déplacement Fox
 def find():
@@ -72,8 +70,11 @@ def find():
     coordF=[]
     affGrid()
 
+Start()
+
 BtnRandom = Button(root,text='Random', command=Start)
 BtnRandom.grid()
 BtnMove = Button(root,text='Move >', command=find)
 BtnMove.grid()
+
 root.mainloop()
