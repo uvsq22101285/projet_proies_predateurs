dcd363f104e2952c9fb1cabf8f57af331609a42c
Eliott MOBRE <eliott.mobre@gmail.com>
2022-04-19 16:18:31 +0200

    flair

--- test.py
+++ test.py
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-'''TEST IMAGE'''
-from tkinter import *  
-from PIL import ImageTk,Image
-''''''''''
-im = Image.open("carré_sol.png")
-print(im.size)
-new_size = im.resize((32,32))
-new_size.show()
-'''''''''
-root = Tk()
-root.title('PythonGuides')
-root.geometry('500x500')
-
-canvas = Canvas(root, width = 640, height = 640,bg='black')
-img = PhotoImage(file ="carré_sol.png")
-img2 = PhotoImage(file ="rabbit.png")
-img3 = PhotoImage(file ="fox.png")
-img4 = PhotoImage(file="tomato.png")
-img5 = PhotoImage(file="migrant.png")
-img6 = PhotoImage(file="police-officer-head.png")
-img7 = PhotoImage(file="water-flask.png")
-img8 = PhotoImage(file="rolling-bomb.png")
-
-for x in range(20):
-    for y in range(20):
-        canvas.create_image(x*64,y*64,image=img,anchor=NW)
-
-canvas.create_image(64,0, image=img3, anchor=NW)
-canvas.create_image(0,0,image=img2, anchor=NW)
-canvas.create_image(0,64,image=img4, anchor=NW)
-canvas.create_image(64,64,image=img5, anchor=NW)
-canvas.create_image(64,128,image=img6, anchor=NW)
-canvas.create_image(128,128,image=img7, anchor=NW)
-canvas.create_image(128,64,image=img8, anchor=NW)
-
-
-canvas.grid()
-
-root.mainloop()
-=======
 #Lib
 from random import randint
 from tkinter import *
@@ -94,30 +53,32 @@ def Check(condition):
                 else:
                     grid[x][y] = []
 
-def flare(x,y):
-    if len(grid[x][y]) == 3:
-        listP=[]
-        for j in range(len(coordP)):
-            dx = coordP[j][0]-x
-            dy = coordP[j][1]-y
-            listP.append(max(abs(dx),abs(dy)))
-        minVal = listP[0]
-        indx = 0
-    #return index min
-        for k in range(len(listP)):
-            if listP[k] < minVal:
-                minVal = listP[k]
-                indx = k 
-        print('cible',coordP[k])        
-    #coord min
-        dx =coordP[indx][0]-x
-        dy =coordP[indx][1]-y
-        dist=[dx,dy]
-        print('dist',dist)
-        for n in range(2):
-            if dist[n]!=0:
-                dist[n]= dist[n]//abs(dist[n]) #deplace de 1
-        return(x+dist[0],y+dist[1])
+def Flair(x,y): #Fonction déplacement renard
+    global coordPro
+    global xpre,ypre
+    #calcul distances
+    dist = []
+    for i in range(len(coordPro)):
+        dist.append([coordPro[i][0]-x,coordPro[i][1]-y])
+    print()
+    print(f'la distance de {[x,y]} entre chaque est {dist}')
+
+    #calcul min dist
+    minVal = max(abs(dist[0][0]),abs(dist[0][1]))
+    print('minimum départ',minVal)
+    minIndx = 0
+    for j in range(len(dist)):
+        if minVal > max(abs(dist[j][0]),abs(dist[j][1])):
+            minVal = max(abs(dist[j][0]),abs(dist[j][1]))
+            minIndx = j
+    print(f'min arrivé {minVal} en {minIndx}')
+    print(f'plus proche ?{coordPro[minIndx]}')
+    for k in range(2):
+        if dist[minIndx][k]!=0:
+            dist[minIndx][k] =dist[minIndx][k]//abs(dist[minIndx][k])
+    xpre = dist[minIndx][0]+x
+    ypre = dist[minIndx][1]+y
+    print(xpre,ypre)
 
 def Start():
     global grid
@@ -135,4 +96,3 @@ BtnMove = Button(root,text='Move >', command=find)
 BtnMove.grid()
 Label(root, text=print(grid))
 root.mainloop()
->>>>>>> ea9ff7b01ef90bc4bedb19552ec18742b386dedd
