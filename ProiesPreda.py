2dc8993060222a5b8bd1841730a23ebfc209ad7f
Jean-Alexandre SUZANNE <jean-alexandre.suzanne@ens.uvsq.fr>
2022-05-08 23:00:11 +0200

    GUI

--- ProiesPreda.py
+++ ProiesPreda.py
@@ -8,18 +8,16 @@ from tkinter import messagebox as box
 #Variables
 
 #Paramètres Partie
-case = 12
+case = 22
 taille_image = 32
-Taille_canvas = case * taille_image
-
+Taille_canvas = (case * taille_image)-2
+vitesse = 60
 
 
 ###########
 #Proies
 #nombres de proies initiales
-Npro = 3
-#fréquences d'apparition des lapins à chaque tour
-Fpro =1
+Npro = 15
 #Durée de vie en tour
 Apro = 5
 
@@ -34,7 +32,7 @@ liste_probis = []
 
 ###########
 #Predateur
-Npre = 1
+Npre = 8
 Apre = 8
 Epre = 9
 xpre = 0
@@ -47,8 +45,10 @@ coordF = []
 ##Variables Temporaires pour les fonctions
 gridtemp = []
 newGrid = []
+condition = False
+compteur = 0
+variable = 0
 
-distMax = 5
 #liste des prédateurs
 liste_preda = []
 
@@ -136,7 +136,10 @@ def NaissanceV2():
                     #condition repro renard
                     if grid[xpro][ypro][1] != Apre and grid[xpro][ypro][2] > 5:
                         Random()
-                        SpawnPre(xpro,ypro,grid) 
+                        SpawnPre(xpro,ypro,grid)
+                
+
+
     affGrid()               
                     
 
@@ -224,27 +227,25 @@ def Flair(x,y): #Fonction déplacement renard
     dist = []
     if liste_pro != [] or liste_probis != []:
         for i in range(len(liste_pro)):
-            if max(abs(liste_pro[i][0]-x),abs(liste_pro[i][1]-y)) <distMax:
-                dist.append([liste_pro[i][0]-x,liste_pro[i][1]-y])
+            dist.append([liste_pro[i][0]-x,liste_pro[i][1]-y])
         for u in range(len(liste_probis)):
-            if max(abs(liste_probis[u][0]-x),abs(liste_probis[u][1]-y)) <distMax:
-                dist.append([liste_probis[u][0]-x,liste_probis[u][1]-y])
-        print(dist)
-        if dist!=[]:       
-            minVal = max(abs(dist[0][0]),abs(dist[0][1]))
-            minIndx = 0
-            for j in range(len(dist)):
-                if minVal > max(abs(dist[j][0]),abs(dist[j][1])):
-                    minVal = max(abs(dist[j][0]),abs(dist[j][1]))
-                    minIndx = j
-            for k in range(2):
-                if dist[minIndx][k]!=0:
-                    dist[minIndx][k] =dist[minIndx][k]//abs(dist[minIndx][k])
-            xpre,ypre = dist[minIndx][0]+x,dist[minIndx][1]+y
-            liste_preda.append([xpre,ypre])
-        else:
-            xpre,ypre = x,y
-            #move random pour renard ici
+            dist.append([liste_probis[u][0]-x,liste_probis[u][1]-y])
+
+        minVal = max(abs(dist[0][0]),abs(dist[0][1]))
+        minIndx = 0
+
+        for j in range(len(dist)):
+            if minVal > max(abs(dist[j][0]),abs(dist[j][1])):
+                minVal = max(abs(dist[j][0]),abs(dist[j][1]))
+                minIndx = j
+        for k in range(2):
+            if dist[minIndx][k]!=0:
+                dist[minIndx][k] =dist[minIndx][k]//abs(dist[minIndx][k])
+        xpre,ypre = dist[minIndx][0]+x,dist[minIndx][1]+y
+        liste_preda.append([xpre,ypre])
+    else:
+        xpre,ypre = x,y
+
 
 def newgrid():
     global newGrid
@@ -296,10 +297,11 @@ def Move():
                     
 
 def Restart(widget):
-    global grid, save
+    global grid, save, compteur
+    compteur = 0
     grid = [[[] for x in range(case)]for y in range(case)]
     widget.grid(row = 1, column=1)
-    root.after(50,Start(BtnStart))
+    Start(BtnStart)
     affGrid()
     save = []
 
@@ -327,7 +329,75 @@ def Next():
     
     liste_pro,liste_probis,liste_preda=[],[],[]
 
+
+def Selection(x):
+    global Npro,Npre,Apro,Apre,Epre,case
+
+    if x == 1 :
+        Npro = int(variable.get())
+    elif x == 2 :
+        Npre = int(variable.get())
+    elif x == 3:
+        Apro = int(variable.get())
+    elif x == 4:
+        Apre = int(variable.get())
+    elif x == 5:
+        Epre = int(variable.get())
+    elif x == 6:
+        case = int(variable.get())
+
+
+
+def Reglages():
+    global variable
+    fenetre = Toplevel()
+    variable = Entry(fenetre, text ="Variable")
+    variable.grid(column=0, row=0)
+    BtnNpro = Button(fenetre, text='Npro', command=lambda: Selection(1))
+    BtnNpro.grid(column =0,row=1)
+    BtnNpre = Button(fenetre, text='Npre', command=lambda: Selection(2))
+    BtnNpre.grid(column =0,row=2)
+    BtnApro = Button(fenetre, text='Apro', command=lambda: Selection(3))
+    BtnApro.grid(column =0,row=3)
+    BtnApro = Button(fenetre, text='Apro', command=lambda: Selection(4))
+    BtnApro.grid(column =0,row=4)
+    BtnEpro = Button(fenetre, text="Epro", command=lambda: Selection(5))
+    BtnEpro.grid(column =0,row=5)
+    BtnEproLabel = Label(fenetre, text="(Au dessus de 9 pas de changement graphique)")
+    BtnEproLabel.grid(column = 1, row=5)
+    BtnCase = Button(fenetre, text='Case', command= lambda: Selection(6))
+    BtnCase.grid(column=0,row=6)
+
+def Auto():
+    global compteur
+    if compteur == 0:
+        Start(BtnStart)
+        compteur = 1
+    Next()
+    if condition == False:
+        root.after(vitesse, Auto)
+
+def Pause(widget):
+    global condition
+    condition = True
+    widget.grid_forget()
+    BtnReprendre.grid(column=2, row=2)
+
+def Reprendre(widget):
+    global condition 
+    condition = False
+    Auto()
+    widget.grid_forget()
+    BtnPause.grid(column=2, row=2)
+
+def RestartAuto():
+    global compteur,condition, grid
+    compteur,condition = 0, False
+    grid = [[[] for x in range(case)]for y in range(case)]
+    Auto()
+
 def Win():
+    global condition
     preda = []
     pro = []
     for x in range(case):
@@ -338,86 +408,96 @@ def Win():
                 preda.append([x,y])
     if pro == []:
         box.showinfo("Victoire","Les Prédateurs ont gagné !")
+        condition = True
     elif preda == []:
         box.showinfo("Victoire","Les Proies ont survécu")
+        condition = True
 
 def affGrid():
     bordureFill(grid,case,'#')
     for x in range(case):
         for y in range(case):
-            canvas.create_image(x*64,y*64,image=sol,anchor=NW)
+            canvas.create_image(x*32,y*32,image=sol,anchor=NW)
             if len(grid[x][y]) == 2:
-                canvas.create_image(x*64,y*64,image=rabbit, anchor=NW)
+                canvas.create_image(x*32,y*32,image=rabbit, anchor=NW)
                 coordR.append([x,y])
                 if grid[x][y][1] == 5:
-                    canvas.create_image(x*64,y*64,image=Image5, anchor=NW)
-                if grid[x][y][1] == 4:
-                    canvas.create_image(x*64,y*64,image=Image4, anchor=NW)
-                if grid[x][y][1] == 3:
-                    canvas.create_image(x*64,y*64,image=Image3, anchor=NW)
-                if grid[x][y][1] == 2:
-                    canvas.create_image(x*64,y*64,image=Image2, anchor=NW)
-                if grid[x][y][1] == 1:
-                    canvas.create_image(x*64,y*64,image=Image1, anchor=NW)
+                    canvas.create_image(x*32,y*32,image=Image5, anchor=NW)
+                elif grid[x][y][1] == 4:
+                    canvas.create_image(x*32,y*32,image=Image4, anchor=NW)
+                elif grid[x][y][1] == 3:
+                    canvas.create_image(x*32,y*32,image=Image3, anchor=NW)
+                elif grid[x][y][1] == 2:
+                    canvas.create_image(x*32,y*32,image=Image2, anchor=NW)
+                elif grid[x][y][1] == 1:
+                    canvas.create_image(x*32,y*32,image=Image1, anchor=NW)
                 
                     
             if len(grid[x][y]) == 3:
-                canvas.create_image(x*64,y*64,image=fox, anchor=NW)
+                canvas.create_image(x*32,y*32,image=fox, anchor=NW)
                 coordF.append([x,y])
                 #Partie Vie
                 if grid[x][y][1] > 8:
-                    canvas.create_image(x*64,y*64,image=Image9, anchor=NW)
-                if grid[x][y][1] == 8:
-                    canvas.create_image(x*64,y*64,image=Image8, anchor=NW)
-                if grid[x][y][1] == 7:
-                    canvas.create_image(x*64,y*64,image=Image7, anchor=NW)
-                if grid[x][y][1] == 6:
-                    canvas.create_image(x*64,y*64,image=Image6, anchor=NW)
-                if grid[x][y][1] == 5:
-                    canvas.create_image(x*64,y*64,image=Image5, anchor=NW)
-                if grid[x][y][1] == 4:
-                    canvas.create_image(x*64,y*64,image=Image4, anchor=NW)
-                if grid[x][y][1] == 3:
-                    canvas.create_image(x*64,y*64,image=Image3, anchor=NW)
-                if grid[x][y][1] == 2:
-                    canvas.create_image(x*64,y*64,image=Image2, anchor=NW)
-                if grid[x][y][1] == 1:
-                    canvas.create_image(x*64,y*64,image=Image1, anchor=NW)
+                    canvas.create_image(x*32,y*32,image=Image9, anchor=NW)
+                elif grid[x][y][1] == 8:
+                    canvas.create_image(x*32,y*32,image=Image8, anchor=NW)
+                elif grid[x][y][1] == 7:
+                    canvas.create_image(x*32,y*32,image=Image7, anchor=NW)
+                elif grid[x][y][1] == 6:
+                    canvas.create_image(x*32,y*32,image=Image6, anchor=NW)
+                elif grid[x][y][1] == 5:
+                    canvas.create_image(x*32,y*32,image=Image5, anchor=NW)
+                elif grid[x][y][1] == 4:
+                    canvas.create_image(x*32,y*32,image=Image4, anchor=NW)
+                elif grid[x][y][1] == 3:
+                    canvas.create_image(x*32,y*32,image=Image3, anchor=NW)
+                elif grid[x][y][1] == 2:
+                    canvas.create_image(x*32,y*32,image=Image2, anchor=NW)
+                elif grid[x][y][1] == 1:
+                    canvas.create_image(x*32,y*32,image=Image1, anchor=NW)
                 #Partie Energie
                 if grid[x][y][2] > 8:
-                    canvas.create_image(x*64,y*64,image=Energie9, anchor=NW)
-                if grid[x][y][2] == 8:
-                    canvas.create_image(x*64,y*64,image=Energie8, anchor=NW)
-                if grid[x][y][2] == 7:
-                    canvas.create_image(x*64,y*64,image=Energie7, anchor=NW)
-                if grid[x][y][2] == 6:
-                    canvas.create_image(x*64,y*64,image=Energie6, anchor=NW)
-                if grid[x][y][2] == 5:
-                    canvas.create_image(x*64,y*64,image=Energie5, anchor=NW)
-                if grid[x][y][2] == 4:
-                    canvas.create_image(x*64,y*64,image=Energie4, anchor=NW)
-                if grid[x][y][2] == 3:
-                    canvas.create_image(x*64,y*64,image=Energie3, anchor=NW)
-                if grid[x][y][2] == 2:
-                    canvas.create_image(x*64,y*64,image=Energie2, anchor=NW)
-                if grid[x][y][2] == 1:
-                    canvas.create_image(x*64,y*64,image=Energie1, anchor=NW)
+                    canvas.create_image(x*32,y*32,image=Energie9, anchor=NW)
+                elif grid[x][y][2] == 8:
+                    canvas.create_image(x*32,y*32,image=Energie8, anchor=NW)
+                elif grid[x][y][2] == 7:
+                    canvas.create_image(x*32,y*32,image=Energie7, anchor=NW)
+                elif grid[x][y][2] == 6:
+                    canvas.create_image(x*32,y*32,image=Energie6, anchor=NW)
+                elif grid[x][y][2] == 5:
+                    canvas.create_image(x*32,y*32,image=Energie5, anchor=NW)
+                elif grid[x][y][2] == 4:
+                    canvas.create_image(x*32,y*32,image=Energie4, anchor=NW)
+                elif grid[x][y][2] == 3:
+                    canvas.create_image(x*32,y*32,image=Energie3, anchor=NW)
+                elif grid[x][y][2] == 2:
+                    canvas.create_image(x*32,y*32,image=Energie2, anchor=NW)
+                elif grid[x][y][2] == 1:
+                    canvas.create_image(x*32,y*32,image=Energie1, anchor=NW)
             if grid[x][y] == '#':
-                canvas.create_image(x*64,y*64,image=mur, anchor=NW)
+                canvas.create_image(x*32,y*32,image=mur, anchor=NW)
     canvas.grid()
     
 
 
 root = Tk()
 root.title('Chasse')
-canvas = Canvas(root, width = Taille_canvas, height = Taille_canvas,bg='green')
+canvas = Canvas(root, width = Taille_canvas, height = Taille_canvas,bg='white')
 canvas.grid(columnspan=1)
-BtnStart = Button(root,text='Start', command=lambda: Start(BtnStart))
-BtnStart.grid(column=0, row=1)
-BtnRestart = Button(root, text='Restart', command=lambda: Restart(BtnStart))
-BtnRestart.grid(row=0, column=1)
-BtnNext = Button(root, text='Next', command=Next)
-BtnRetour = Button(root, text='Retour', command=Retour).grid(column=1,row=1)
+BtnStart = Button(root,text='Simulation Manuelle', command=lambda: Start(BtnStart))
+BtnStart.grid(column=1, row=1)
+BtnRestart = Button(root, text='Relance Manuelle', command=lambda: Restart(BtnStart))
+BtnRestart.grid(row=3, column=1)
+BtnNext = Button(root, text='Tour Suivant', command=Next)
+BtnRetour = Button(root, text='Retour (seulement manuelle)', command=Retour).grid(column=0,row=2)
+BtnAuto = Button(root, text= 'Simulation',command=Auto).grid(column=2,row=1)
+BtnReglages = Button(root, text= 'Réglages',command=Reglages).grid(column=1,row=0)
+BtnPause = Button(root,text= 'Pause',command=lambda: Pause(BtnPause))
+BtnPause.grid(column=2,row=2)
+BtnReprendre = Button(root,text= 'Reprendre',command=lambda: Reprendre(BtnReprendre))
+BtnRestartAuto = Button(root,text= 'Relancer la simulation',command=RestartAuto)
+BtnRestartAuto.grid(column=2,row=3)
+
 
 #Images Projet
 sol = PhotoImage(file ="carré_sol.png")
